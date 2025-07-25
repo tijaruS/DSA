a57e40f16065da7110e18d6f2b6a7d6651f9c8ba
tijaruS <surajitdalelpara25@gmail.com>
2025-07-25 20:59:27 +0530

    add more binary search

--- BinarySearch/findTheSmallestDivisorGivenATreshold.cpp
+++ BinarySearch/findTheSmallestDivisorGivenATreshold.cpp
@@ -1,4 +1,14 @@
+#include <algorithm>
 #include <bits/stdc++.h>
+
+int check(std::vector<int> &arr, int mid) {
+
+    int sum = 0;
+    for (int i = 0; i < arr.size(); i++) {
+        sum += (arr[i] + mid - 1) / mid;
+    }
+    return sum;
+}
 
 int main() {
     int n, threshold;
@@ -7,6 +17,18 @@ int main() {
     for (int i = 0; i < n; i++) {
         std::cin >> arr[i];
     }
+    int low = 1;
+    int high = *std::max_element(arr.begin(), arr.end());
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (check(arr, mid) > threshold) {
+
+            low = mid + 1;
+
+        } else
+            high = mid - 1;
+    }
+    std::cout << low << std::endl;
 
     return 0;
 }
